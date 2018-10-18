03cd5736803989826fb36e9108d0a129bc38a2ee
unknown <talktosushmagoatam@gmail.com>
2018-10-18 23:56:00 +0530

    homecontroller added

---
+++ src/com/onlineshopping/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.onlineshopping.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/")
+	public String home() {
+		return "home";
+	}
+}
